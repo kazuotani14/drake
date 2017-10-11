5ad877f835cd2713a70a487652c12673b1eef8d3
Kazu Otani <kazuotani14@gmail.com>
2017-10-11 11:49:02 +0200

    Add collision filter groups and contact publishing to human sim

--- drake/examples/human_model/human_passive_simulation.cc
+++ drake/examples/human_model/human_passive_simulation.cc
@@ -8,6 +8,8 @@
 #include "drake/common/find_resource.h"
 #include "drake/common/text_logging.h"
 #include "drake/lcm/drake_lcm.h"
+#include "drake/systems/lcm/lcm_publisher_system.h"
+#include "drake/systems/lcm/lcm_subscriber_system.h"
 #include "drake/multibody/parsers/urdf_parser.h"
 #include "drake/multibody/rigid_body_plant/drake_visualizer.h"
 #include "drake/multibody/rigid_body_plant/rigid_body_plant.h"
@@ -16,6 +18,8 @@
 #include "drake/systems/analysis/simulator.h"
 #include "drake/systems/framework/diagram_builder.h"
 #include "drake/systems/primitives/constant_vector_source.h"
+#include "drake/lcmt_contact_results_for_viz.hpp"
+#include "drake/multibody/rigid_body_plant/contact_results_to_lcm.h"
 
 DEFINE_double(simulation_sec, std::numeric_limits<double>::infinity(),
               "Number of seconds to simulate.");
@@ -29,63 +33,73 @@ int DoMain(int argc, char* argv[]) {
 
   drake::lcm::DrakeLcm lcm;
 
-  // Construct the tree for the human model.
-  auto tree = std::make_unique<RigidBodyTree<double>>();
+  auto tree_ptr = std::make_unique<RigidBodyTree<double>>();
   parsers::urdf::AddModelInstanceFromUrdfFileToWorld(
-      //      FindResourceOrThrow("drake/examples/acrobot/Acrobot.urdf"),
-      FindResourceOrThrow(
-          "drake/examples/valkyrie/urdf/urdf/"
-              "valkyrie_A_sim_drake_one_neck_dof_wide_ankle_rom.urdf"),
-//  FindResourceOrThrow(
-//          "drake/examples/human_model/models/simplified_human_model.urdf"),
-      multibody::joints::kRollPitchYaw, tree.get());
+//            FindResourceOrThrow("drake/examples/acrobot/Acrobot.urdf"),
+//      FindResourceOrThrow(
+//          "drake/examples/valkyrie/urdf/urdf/"
+//              "valkyrie_A_sim_drake_one_neck_dof_wide_ankle_rom.urdf"),
+  FindResourceOrThrow(
+          "drake/examples/human_model/models/simplified_human_model.urdf"),
+      multibody::joints::kRollPitchYaw, tree_ptr.get());
 
-  const double terrain_size = 100;
-  const double terrain_depth = 10;
-  multibody::AddFlatTerrainToWorld(tree.get(), terrain_size, terrain_depth);
-
-  int n_actuators = tree->get_num_actuators();
-
-  drake::log()->info("Number of bodies: {}", tree->get_num_bodies());
+  int n_actuators = tree_ptr->get_num_actuators();
+  drake::log()->info("Number of bodies: {}", tree_ptr->get_num_bodies());
   drake::log()->info("Number of actuators: {}", n_actuators);
-  drake::log()->info("Mass: {}", tree->getMass());
+  drake::log()->info("Mass: {}", tree_ptr->getMass());
 
-  systems::DiagramBuilder<double> diagram_builder;
-  auto plant = diagram_builder.AddSystem<systems::RigidBodyPlant<double>>(
-      std::move(tree));
+  multibody::AddFlatTerrainToWorld(tree_ptr.get(), 100., 10.);
+
+  systems::DiagramBuilder<double> builder;
+  auto plant = builder.AddSystem<systems::RigidBodyPlant<double>>(
+      std::move(tree_ptr));
   plant->set_name("plant");
+  const auto& tree = plant->get_rigid_body_tree();
 
   systems::DrakeVisualizer& viz_publisher =
-      *diagram_builder.template AddSystem<systems::DrakeVisualizer>(
+      *builder.template AddSystem<systems::DrakeVisualizer>(
           plant->get_rigid_body_tree(), &lcm);
   viz_publisher.set_name("visualizer_publisher");
-  diagram_builder.Connect(plant->state_output_port(),
+  builder.Connect(plant->state_output_port(),
                           viz_publisher.get_input_port(0));
 
   // Send the robot's actuators zeros in absence of a controller.
-  if (n_actuators) {
-    std::cout << "Setting constant actuator torque" << std::endl;
-    int n_torques = plant->actuator_command_input_port().size();
-    VectorX<double> constant_torques = VectorX<double>::Constant(n_torques, 0.0);
-    auto constant_torque_source =
-        diagram_builder.AddSystem<systems::ConstantVectorSource<double>>(
-            constant_torques);
-    diagram_builder.Connect(constant_torque_source->get_output_port(),
-                            plant->actuator_command_input_port());
-  }
+  std::cout << "Setting constant actuator torque" << std::endl;
+  int n_torques = plant->actuator_command_input_port().size();
+  VectorX<double> constant_torques = VectorX<double>::Constant(n_torques, 0.0);
+  auto constant_torque_source =
+      builder.AddSystem<systems::ConstantVectorSource<double>>(
+          constant_torques);
+  builder.Connect(constant_torque_source->get_output_port(),
+                          plant->actuator_command_input_port());
 
-  // Set contact parameters that support gripping.
-  const double kStaticFriction = 1;
-  const double kDynamicFriction = 5e-1;
-  const double kStictionSlipTolerance = 1e-3;
-  plant->set_friction_contact_parameters(kStaticFriction, kDynamicFriction,
-                                         kStictionSlipTolerance);
-
-  const double kStiffness = 1000;
-  const double kDissipation = 100;
+  // Contact parameters
+  const double kStiffness = 100000;
+  const double kDissipation = 5.0;
+  const double kStaticFriction = 0.9;
+  const double kDynamicFriction = 0.5;
+  const double kStictionSlipTolerance = 0.01;
   plant->set_normal_contact_parameters(kStiffness, kDissipation);
+  plant->set_friction_contact_parameters(kStaticFriction, kDynamicFriction,
+                                          kStictionSlipTolerance);
 
-  auto diagram = diagram_builder.Build();
+  // Set up contact force visualization
+  auto contact_viz =
+      builder.template AddSystem<systems::ContactResultsToLcmSystem<double>>(
+          tree);
+  contact_viz->set_name("contact_viz");
+  auto contact_results_publisher = builder.AddSystem(
+      systems::lcm::LcmPublisherSystem::Make<lcmt_contact_results_for_viz>(
+          "CONTACT_RESULTS", &lcm));
+  contact_results_publisher->set_name("contact_results_publisher");
+
+  // Contact results to lcm msg.
+  builder.Connect(plant->contact_results_output_port(),
+                  contact_viz->get_input_port(0));
+  builder.Connect(contact_viz->get_output_port(0),
+                  contact_results_publisher->get_input_port(0));
+
+  auto diagram = builder.Build();
   systems::Simulator<double> simulator(*diagram);
 
   auto context = simulator.get_mutable_context();
@@ -96,10 +110,9 @@ int DoMain(int argc, char* argv[]) {
   VectorX<double> x0 = VectorX<double>::Zero(state_size);
 
   // First 6 entries are floating-base
-  x0[2] = 1.0;
-  x0[4] = 1.0;
-
-//  x0[5] = 0.5;
+  x0[2] = 4.0;
+//  x0[3] = 1.0;
+//  x0[4] = 1.0;
 
   simulator.get_mutable_context()
       ->get_mutable_continuous_state_vector()
--- test_scripts/test_human_sim.sh
+++ test_scripts/test_human_sim.sh
@@ -1,5 +1,5 @@
 echo "Starting Drake visualizer"
 ../bazel-bin/tools/drake_visualizer&
-sleep 2
+sleep 5
 echo "Starting human model simulation"
 ../bazel-bin/drake/examples/human_model/human_passive_simulation
